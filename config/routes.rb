498efb7f03910e4e32110c256e5259e4451ed2d3
Tim Robinson <tim.robinson@powershop.co.nz>
2014-08-21 16:15:38 +1200

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ SampleApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
+  resources :users
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
