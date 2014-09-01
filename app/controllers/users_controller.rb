3037906f4b8d46f59e28c57aaffcdf5b4225e667
Tim Robinson <tim.robinson@powershop.co.nz>
2014-09-01 15:27:50 +1200

    add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -52,12 +53,7 @@ class UsersController < ApplicationController
   	end
 
     #before filters
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+    
 
 
     def correct_user
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -93,11 +93,20 @@ end
 
   describe "Profile Page" do
   	let(:user) { FactoryGirl.create(:user) }
-  	before { visit user_path(user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
-  	it { should have_content(user.name) }
-  	it { should have_title(user.name) }
-	end
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+  end
 
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
